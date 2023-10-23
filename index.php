9979e338cadae7e4bb0982021579d59d531ecbde
Sahil Patel <jonyydeep555@gmail.com>
2023-10-23 12:46:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <div>
+        <h1>Writing 3 Facebook Marketing Captions for</h1>
+    </div>
+    <div>
+        <form action="ai.php" method="post">
+            <div>
+                <input type="text" name="prompt" placeholder="shoe, burger, etc" />
+            </div>
+            <br>
+            <div>
+                <input type="submit" value="Generate" />
+            </div>
+        </form>
+    </div>
+    <hr> -->
+    <center>
+    <div>
+        <h1>Image Generation By AI</h1>
+    </div>
+    <div>
+        
+        <form action="image-ai.php" method="post">
+            <div>
+                <input type="text" name="prompt" placeholder="search image" />
+            </div>
+            <br>
+            <div>
+                <input type="submit" value="Generate Image" />
+            </div>
+        </form>
+        </center>
+    </div>
+</body>
+</html>
